c0077887b73072fdaca4160650b388aaad10a9ea
dxpelou <la.olu@live.co.uk>
2017-12-05 22:43:25 +0000

    isolation game

---
+++ AIND-Isolation-master/agent_test.py
@@ -0,0 +1,39 @@
+"""This file is provided as a starting template for writing your own unit
+tests to run and debug your minimax and alphabeta agents locally.  The test
+cases used by the project assistant are not public.
+"""
+
+import unittest
+
+import isolation
+import game_agent
+import sample_players
+
+from importlib import reload
+
+
+class IsolationTest(unittest.TestCase):
+    """Unit tests for isolation agents"""
+
+    def setUp(self):
+        reload(game_agent)
+        self.player1 = game_agent.MinimaxPlayer()
+        self.player2 = sample_players.GreedyPlayer()
+        self.game = isolation.Board(self.player1, self.player2)
+
+
+
+    def test_stubBoard(self):
+        stub_board_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1,
+                            1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1,
+                            0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 13, 61]
+
+        self.game._board_state = stub_board_state
+
+        move = self.player1.get_move(self.game, 10)
+        print(move)
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
